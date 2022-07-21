297e3759e09b40cf9bc7b12e6775b7276b6fcc6a
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-07-21 20:37:20 +0300

    Done.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ function Navbar({ setIsLoggedIn }) {
   return (
     <div>
       <NavLink
-        to="/"
+        to="/home"
         /* set exact so it knows to only set activeStyle when route is deeply equal to link */
         exact
         /* add styling to Navlink */
